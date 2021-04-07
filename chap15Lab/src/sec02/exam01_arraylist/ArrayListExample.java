d25c7617414ddf682de3ea284ecf2e0c3621cd9d
minjong8282 <minjong8282@naver.com>
2021-04-07 11:36:20 +0900

    chap15 예제

---
+++ chap15Lab/src/sec02/exam01_arraylist/ArrayListExample.java
@@ -0,0 +1,39 @@
+package sec02.exam01_arraylist;
+
+import java.util.*;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Java");  //index0
+		list.add("JDBC");   //index1
+		list.add("Servlet/JSP"); //index2
+		list.add(2, "Database"); //index2에 들어가서 기존에 값들은 한칸씩  뒤로 밀려난다
+		list.add("iBATIS"); //index4
+
+		int size = list.size();  //size는 5
+		System.out.println("총 객체수: " + size);		
+		System.out.println();
+		
+		String skill = list.get(2);
+		System.out.println("2: " + skill);
+		System.out.println();
+
+		for(int i=0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+		System.out.println();
+				
+		list.remove(2);
+		list.remove(2);
+		list.remove("iBATIS");		
+		
+		for(int i=0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+	}
+}
+
